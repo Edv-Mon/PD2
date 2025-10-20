e2c93ffe005c58792d3095b649496566cf8d2d1b
montv <edvinas.montvila@knf.stud.vu.lt>
2025-10-20 16:51:22 +0300

    commit

---
+++ app/src/main/java/com/example/pd2/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.pd2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.pd2.utils.TextCounter;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvCountResult;
+    EditText edUserInput;
+    Button button;
+    Spinner spSelectionOp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        this.tvCountResult = findViewById(R.id.tvCountResult);
+        this.edUserInput = findViewById(R.id.edUserInput);
+
+        this.spSelectionOp = findViewById(R.id.spSelectionOp);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.count_options,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spSelectionOp.setAdapter(adapter);
+    }
+
+    public void onBtnCountClick(View view) {
+        if(this.spSelectionOp.getSelectedItem().toString().equalsIgnoreCase("Number of characters")) {
+            String userPhrase = this.edUserInput.getText().toString();
+            int charsCount = TextCounter.getCharsCount(userPhrase);
+            this.tvCountResult.setText(String.valueOf(charsCount));
+        }
+        else{
+            this.tvCountResult.setText("Invalid option");
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/pd2/utils/TextCounter.java
@@ -0,0 +1,9 @@
+package com.example.pd2.utils;
+
+public class TextCounter {
+    public static int getCharsCount(String userInput){
+        return userInput.length();
+
+    }
+}
+
